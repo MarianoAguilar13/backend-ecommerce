a7a3f7215c662eaf7a55a849d25dad5d501c552f
Mariano <mariano_aguilar_13@hotmail.com>
2023-04-06 16:37:37 -0300

    Agregando cors a /auth

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -2,6 +2,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import methods from "micro-method-router";
 import { sendCode } from "controllers/auth";
 import * as yup from "yup";
+import Cors from "cors";
+import { runMiddleware } from "../cors";
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+});
 
 let bodySchema = yup.object().shape({
   email: yup.string().email().required(),
@@ -17,6 +24,8 @@ let bodySchema = yup.object().shape({
 
 export default methods({
   async post(req: NextApiRequest, res: NextApiResponse) {
+    // Run the middleware
+    await runMiddleware(req, res, cors);
     //chequeando el req y req.body
     try {
       await bodySchema.validate(req.body);
