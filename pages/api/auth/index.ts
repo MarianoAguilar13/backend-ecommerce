b886334f421dd47d6079012062a4b6704823997c
Mariano <mariano_aguilar_13@hotmail.com>
2023-04-06 18:07:32 -0300

    Agregando cors a /auth

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import methods from "micro-method-router";
 import { sendCode } from "controllers/auth";
 import * as yup from "yup";
-import { handlerCORS } from "../cors";
+import corsMiddleware from "../mddleware-cors";
 
 let bodySchema = yup.object().shape({
   email: yup.string().email().required(),
@@ -15,31 +15,9 @@ let bodySchema = yup.object().shape({
   email: yup.string().required(),
 }).noUnknown(true).strict;
 */
-
+/*
 export default methods({
   async post(req: NextApiRequest, res: NextApiResponse) {
-    //chequeando el req y req.body
-    /*
-    const apiEndPoint = async (req, res) => {
-      try {
-        await bodySchema.validate(req.body);
-        const response = await sendCode(req.body.email);
-        if (response.mensaje) {
-          res.send({ message: response.message });
-        } else {
-          res.send({ error: response.error });
-        }
-      } catch (error) {
-        res.send({
-          message:
-            "Faltan datos en el body que son necesarios para la llamada a la api",
-          error,
-        });
-      }
-    };*/
-
-    //handlerCORS(apiEndPoint);
-
     try {
       await bodySchema.validate(req.body);
       const response = await sendCode(req.body.email);
@@ -57,3 +35,32 @@ export default methods({
     }
   },
 });
+*/
+
+async function auth(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    await bodySchema.validate(req.body);
+    const response = await sendCode(req.body.email);
+    if (response.mensaje) {
+      res.send({ message: response.message });
+    } else {
+      res.send({ error: response.error });
+    }
+  } catch (error) {
+    res.send({
+      message:
+        "Faltan datos en el body que son necesarios para la llamada a la api",
+      error,
+    });
+  }
+}
+
+const handler = methods({
+  post: auth,
+});
+
+const corsHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  return await corsMiddleware(req, res, handler);
+};
+
+export default corsHandler;
